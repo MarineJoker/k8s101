fff467e589ec3e183cc080a74c69ab5ba3af5b67
MarineJoker <hellowjbb@gmail.com>
2023-05-28 23:57:44 +0800

    Create httpServer

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	reHeader := r.Header
+	rpReader := w.Header()
+	rpReader = reHeader
+	rpReader.Write(w)
+	fmt.Fprintf(w, "这是主页")
+}
+
+func ok(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "访问成功")
+}
+
+
+func main() {
+	http.HandleFunc("/", home)
+	http.HandleFunc("localhost/healthz", ok)
+	http.ListenAndServe(":8080", nil)
+}
+
